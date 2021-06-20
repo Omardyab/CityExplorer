5c9af9a527134567459e488d0a178079475c047c
Omardyab <omarzadyab@gmail.com>
2021-06-20 18:44:03 +0300

    fixing class7 problem with new branch

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 // import Card from 'react-bootstrap/Card';
 // import ListGroup from 'react-bootstrap/ListGroup'
 // import Weather from './comp/Weather';
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -22,18 +20,6 @@ class App extends React.Component {
     }
   }
 
-  // getlocresult=async()=>{
-
-
-  //  let url=`https://eu1.locationiq.com/v1/search.php?key=pk.69fbe9b91e039f3c21705cb4442c99ef&q=amman&format=json`
-  //   // console.log(url,city)
-
-  //   let Result=await axios.get(url) ;
-  //   console.log(Result.data);
-  //   this.setState({
-  //     locData:Result.data[0]
-  //   })
-  // }
   getloc = async (event) => {
     event.preventDefault();
     let city = event.target.cityname.value;
@@ -84,7 +70,7 @@ class App extends React.Component {
         err: 'This is a bad response Error 404',
         disErr: true,
         disweather: false,
-        
+
 
       })
     }
@@ -99,23 +85,12 @@ class App extends React.Component {
           <input type='text' placeholder='cityname' name='cityname' />
           <input type='submit' value='Search' />
         </form>
-        {/* <form onSubmit={this.getloc}>
-       <input type='text' placeholder='City Name' name='cityName'/>
-       <input type='submit' value='Search'/>
-      </form > */}
-        {/* <button onClick={this.getlocresult}>This will show data realted to Amman only </button> */}
+
         <h3>City is : {this.state.locData.display_name}</h3>
         {/* <h2>Icon : {this.state.locData.icon}</h2> */}
         <h3>lat is : {this.state.locData.lat}</h3>
         <h3>long is : {this.state.locData.lon}</h3>
         <p>{this.state.disErr && this.state.err}</p>
-        {/* <p>{this.props.weatherData.date}</p>
-                        <p>{this.props.weatherData.description}</p> */}
-        {/* <Weather weather={this.state.weatherData}/>  */}
-
-        {/* <img src={'https://maps.locationiq.com/v3/staticmap?key=pk.69fbe9b91e039f3c21705cb4442c99ef&center=31.9515694,35.9239625'} alt='map'/> */}
-        {/* <Weather weatherData={this.state.weatherData} />
-          {  */}
 
         {this.state.disweather &&
           this.state.weatherData.map(value => {
@@ -136,12 +111,41 @@ class App extends React.Component {
 }
 export default App;
 
-   // console.log(city);
-    // console.log(url) 
-    // console.log(Result.data[0].lat);
-    // console.log(Result.data[0].lon);
-    // console.log(Result.data[0]);
-    // let serverRoot=process.env.REACT_APP_SERVER || 3000;
-    // const weatherurl=`https://eu1.locationiq.com/v1/search.php?key=pk.69fbe9b91e039f3c21705cb4442c99ef&q=${city}&format=json`
-    // //`https://class-7-city-explorer.herokuapp.com/weather?cityname=${this.state.}`;
-    // const weatherDaily= await axios.get(weatherurl);
+// console.log(city);
+// console.log(url) 
+// console.log(Result.data[0].lat);
+// console.log(Result.data[0].lon);
+// console.log(Result.data[0]);
+// let serverRoot=process.env.REACT_APP_SERVER || 3000;
+// const weatherurl=`https://eu1.locationiq.com/v1/search.php?key=pk.69fbe9b91e039f3c21705cb4442c99ef&q=${city}&format=json`
+// //`https://class-7-city-explorer.herokuapp.com/weather?cityname=${this.state.}`;
+// const weatherDaily= await axios.get(weatherurl);
+
+
+// getlocresult=async()=>{
+
+
+//  let url=`https://eu1.locationiq.com/v1/search.php?key=pk.69fbe9b91e039f3c21705cb4442c99ef&q=amman&format=json`
+//   // console.log(url,city)
+
+//   let Result=await axios.get(url) ;
+//   console.log(Result.data);
+//   this.setState({
+//     locData:Result.data[0]
+//   })
+// }
+
+{/* <p>{this.props.weatherData.date}</p>
+                        <p>{this.props.weatherData.description}</p> */}
+{/* <Weather weather={this.state.weatherData}/>  */ }
+
+{/* <img src={'https://maps.locationiq.com/v3/staticmap?key=pk.69fbe9b91e039f3c21705cb4442c99ef&center=31.9515694,35.9239625'} alt='map'/> */ }
+{/* <Weather weatherData={this.state.weatherData} />
+          {  */}
+// eslint-disable-next-line no-lone-blocks
+{/* <form onSubmit={this.getloc}>
+       <input type='text' placeholder='City Name' name='cityName'/>
+       <input type='submit' value='Search'/>
+      </form > */}
+{/* <button onClick={this.getlocresult}>This will show data realted to Amman only </button> */ }
+
